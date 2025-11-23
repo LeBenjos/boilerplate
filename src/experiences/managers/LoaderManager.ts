649f7518061afc2c728812b0f2bde5a0f33adb30
LeBenjos <benjamin.schinkel01@outlook.fr>
2025-11-23 16:13:00 +0100

    feat: loader-progress

--- src/experiences/managers/LoaderManager.ts
+++ src/experiences/managers/LoaderManager.ts
@@ -4,6 +4,8 @@ import ThreeAssetsManager from "./ThreeAssetsManager";
 
 export default class LoaderManager {
     private static _LoadingBar: HTMLDivElement = document.querySelector(".loading-bar")!;
+    private static _LoadingProgress: HTMLDivElement = document.querySelector(".loading-progress")!;
+    private static _LoadingNumber: HTMLSpanElement = LoaderManager._LoadingProgress.querySelector(".loading-number")!;
     private static _TotalAssetsToLoad: number = 0;
     private static _AssetsLoaded: number = 0;
 
@@ -16,6 +18,7 @@ export default class LoaderManager {
     public static LoadAllAssets(): void {
         LoaderManager._TotalAssetsToLoad += ThreeAssetsManager.ToLoadCount;
         ThreeAssetsManager.LoadAssets();
+        LoaderManager._LoadingProgress.style.opacity = "1";
     }
 
     private static _onLoad = (): void => {
@@ -23,6 +26,7 @@ export default class LoaderManager {
 
         const progress = LoaderManager._AssetsLoaded / LoaderManager._TotalAssetsToLoad * 100;
         LoaderManager._LoadingBar.style.transform = `translateY(-50%) scaleX(${progress / 100})`;
+        LoaderManager._LoadingNumber.textContent = Math.round(progress).toString();
 
         LoaderManager._OnFinishLoad();
     }
@@ -38,6 +42,8 @@ export default class LoaderManager {
         LoaderManager._LoadingBar.classList.add("ended");
         LoaderManager._LoadingBar.style.transform = '';
         LoaderManager._LoadingBar.removeEventListener(DomEvent.TRANSITION_END, LoaderManager._OnBarTransitionEnd);
+        LoaderManager._LoadingProgress.classList.add("ended");
+        LoaderManager._LoadingProgress.style.opacity = '';
     }
 
     //#region Getters
